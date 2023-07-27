7f9a89d69687aad9526c7957e79a4cc3749c63fd
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-07-27 11:43:15 +0100

    add

---
+++ C 01/EX02/ft_swap_main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void ft_swap(int *a, int *b)
+{
+    int tmp;
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+int main(){
+    int x;
+    int y;
+
+    x= 2;
+    y= 3;
+    ft_swap(&x, &y);
+    print("%d \n", x);
+    printf("%d ", y);
+}
